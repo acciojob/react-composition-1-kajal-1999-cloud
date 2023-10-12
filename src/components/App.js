a30fdb1a79c67e0e45680c9a6844f9d027b72f55
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-10-12 06:04:03 +0000

    j

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 
 import React from "react";
 import './../styles/App.css';
+import Tabs from './tabs'
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <Tabs/>
     </div>
   )
 }
